fbce6bbf17032faedd7ea7f86ed77d12f48aa305
aboud20-meet <aboud20@meet.mit.edu>
2018-08-08 16:24:34 +0300

    routing

--- exercises/app.py
+++ exercises/app.py
@@ -4,7 +4,10 @@ app = Flask(__name__)
 
 @app.route('/')
 def sign_up():
-    return render_template( 'hello1.html')
+    exersices=["PULL UP" , "PUSH-UP ", "MUCLE UP ", "SIT UP ", "PLANCHE ", "SQUAT , PISTOL SQUAT  " , "HANDSTAND PUSH-UP","FRONT LEVER , BACK LEVER", "SUPERMAN PUSH-UP","HUMAN FLAG" ]
+    return render_template(
+     "hello1.html",
+     exersices=exersices)
 
 @app.route('/home')
 def home_page():
