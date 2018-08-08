5ea8ac1080f5e29504dfd7a649181a48d83ca95d
aboud20-meet <aboud20@meet.mit.edu>
2018-08-08 12:44:30 +0300

    routing

--- exercises/app.py
+++ exercises/app.py
@@ -3,7 +3,11 @@ from flask import Flask, render_template, url_for
 app = Flask(__name__)
 
 @app.route('/')
-def home():
-    return 'START HERE: CHANGE ME'
+def sign_up():
+    return render_template( 'hello1.html')
+
+@app.route('/home')
+def home_page():
+    return render_template( 'hello.html')
 
 app.run(debug=True)
